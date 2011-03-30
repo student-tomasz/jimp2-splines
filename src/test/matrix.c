a02f367e6660eed90108694cd7a0c1aca855f48b
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-03-31 01:19:00 +0200

    add test with not trivial case for matrix_gauss()

--- src/test/matrix.c
+++ src/test/matrix.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "minunit.h"
 #include "../matrix.h"
 
@@ -88,12 +89,40 @@ static int bigger_gauss_with_no_gotchas()
   return 0;
 }
 
+static int gauss_with_gotchas()
+{
+  double ta[] = {
+    6, -3, 2, 1, -1, 1,
+    -3, -7, 0, 4, -2, 1,
+    4, -3, 6, -1, 2, 1,
+    2, 4, 5, -7, -3, 2,
+    -1, 5, -4, 0, 8, -2,
+    3, 0, 4, -2, 5, -6
+  };
+  matrix_t *A = matrix_new(ta, 6, 6);
+  double tb[] = {11, -5, 28, -6, 25, -4};
+  matrix_t *b = matrix_new(tb, 6, 1);
+  double tx[] = {1, 2, 3, 4, 5, 6};
+  matrix_t *should_be_x = matrix_new(tx, 6, 1);
+
+  /* printf("%s:%d: test %s says:\n", __FILE__, __LINE__, __func__);*/
+  matrix_t *x = matrix_gauss(A, b);
+  mu_assert(matrix_is_equal(x, should_be_x));
+
+  matrix_free(x);
+  matrix_free(should_be_x);
+  matrix_free(b);
+  matrix_free(A);
+  return 0;
+}
+
 int all_matrix_tests()
 {
   int (*tests[])() = {
     create_the_simplest_matrix,
     gauss_with_no_gotchas,
-    bigger_gauss_with_no_gotchas
+    bigger_gauss_with_no_gotchas,
+    gauss_with_gotchas
   };
 
   int i;
