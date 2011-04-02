d82ada1123a560472fc0112300715e50d55ee29a
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-04-02 19:00:04 +0200

    add printing and comparing for points

--- src/point.c
+++ src/point.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "io.h"
+#include "epsilon.h"
 #include "point.h"
 
 point_t *point_new(double x, double y)
@@ -14,4 +17,16 @@ void point_free(point_t *p)
   if (p) {
     free(p);
   }
+}
+
+char *point_to_str(point_t *p)
+{
+  char *str = malloc(sizeof(*str) * (MAX_STR_LENGTH + 1));
+  sprintf(str, "(%lg; %lg)", p->x, p->y);
+  return str;
+}
+
+int point_is_equal(point_t *lp, point_t *rp)
+{
+  return is_equal(lp->x, rp->x) && is_equal(lp->y, rp->y);
 }
--- src/point.h
+++ src/point.h
@@ -8,5 +8,7 @@ typedef struct point_s {
 
 point_t *point_new(double x, double y);
 void point_free(point_t *p);
+char *point_to_str(point_t *p);
+int point_is_equal(point_t *lp, point_t *rp);
 
 #endif /* _POINT_H */
