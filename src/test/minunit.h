30149ab8d113a35102afde00a4a0870707d25504
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-04-03 01:17:52 +0200

    make assertion the final test result

--- src/test/minunit.h
+++ src/test/minunit.h
@@ -7,7 +7,7 @@
 
 extern int tests_run;
 
-#define mu_assert(assertion) do { ++tests_run; printf("%s:%d: %s: %s\n", __FILE__, __LINE__, assertion ? "success" : "fail", __func__); if (!assertion) return 1; } while (0)
+#define mu_assert(assertion) do { ++tests_run; printf("%s:%d: %s: %s\n", __FILE__, __LINE__, assertion ? "success" : "fail", __func__); return !assertion; } while (0)
 #define mu_run_test(test) do { int msg = test(); if (msg) return 1; } while (0)
 
 #endif /* _MINUNIT_H */
