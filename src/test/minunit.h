3921885889b2f273b5dcc52e5779cb5ee55c6759
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-04-03 00:13:00 +0200

    be more verbose when test succeeds

--- src/test/minunit.h
+++ src/test/minunit.h
@@ -7,7 +7,7 @@
 
 extern int tests_run;
 
-#define mu_assert(assertion) do { ++tests_run; if (!(assertion)) { printf("%s:%d: fail: %s\n", __FILE__, __LINE__, __func__); return 1; } } while (0)
+#define mu_assert(assertion) do { ++tests_run; printf("%s:%d: %s: %s\n", __FILE__, __LINE__, assertion ? "success" : "fail", __func__); if (!assertion) return 1; } while (0)
 #define mu_run_test(test) do { int msg = test(); if (msg) return 1; } while (0)
 
 #endif /* _MINUNIT_H */
