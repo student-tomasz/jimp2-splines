64323c898d08e94b3684ff495e62e48c3028d4cb
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-04-05 00:06:07 +0200

    add spline test

--- src/test/spline.c
+++ src/test/spline.c
@@ -31,13 +31,38 @@ static int basic_spline()
   mu_assert(1);
 }
 
+static int another_basic_spline()
+{
+  int m = 2;
+  list_t *nodes = list_add(NULL, point_new(1, 1));
+  list_add(nodes, point_new(2, 5));
+  list_add(nodes, point_new(3, 4));
+
+  list_t *splines = spline_interpolate(nodes);
+
+  polynomial_t **should_be_splines = malloc(sizeof(*should_be_splines) * m);
+  double c0[] = { -3.0, 1.5, 3.75, -1.25 };
+  should_be_splines[0] = polynomial_new(c0, 3);
+  double c1[] = { -23.0, 31.5, -11.25, 1.25 };
+  should_be_splines[1] = polynomial_new(c1, 3);
+
+  int i;
+  list_t *spline = NULL;
+  for (i = 0, spline = splines; i < m; ++i, spline = spline->next) {
+    if (!polynomial_is_equal((polynomial_t *)spline->data, should_be_splines[i]))
+      mu_assert(0);
+  }
+
+  mu_assert(1);
+}
 int all_spline_tests()
 {
   options_init();
   options->quiet = 1;
 
   int (*tests[])() = {
-    basic_spline
+    basic_spline,
+    another_basic_spline
   };
 
   int i;
