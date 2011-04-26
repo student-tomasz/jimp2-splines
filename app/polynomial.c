e7853be05f25fd39e3dbda1657b34fbebe9ee50d
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-04-26 20:35:25 +0200

    same goes for printing polynomials

--- app/polynomial.c
+++ app/polynomial.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include "epsilon.h"
 #include "polynomial.h"
-#include "io.h"
+#include "epsilon.h"
 
 polynomial_t *polynomial_new(double *coeffs, int degree)
 {
@@ -46,33 +45,19 @@ int polynomial_is_equal(polynomial_t *lp, polynomial_t *rp) {
 
 char *polynomial_to_str(polynomial_t *p)
 {
-  char *str = malloc(sizeof(*str) * (MAX_STR_LENGTH + 1));
-  char *tmp = malloc(sizeof(*tmp) * (MAX_STR_LENGTH + 1));
-
-  str[0] = '\0';
+  char *str = "";
   int i;
   for (i = 0; i <= p->d; ++i) {
-    sprintf(tmp, "%10.10g", p->c[i]);
-    strcat(str, tmp);
-
+    asprintf(&str, "%s %+g", str, p->c[i]);
     if (i == 0)
       ;
     else if (i == 1) {
-      sprintf(tmp, "*x");
-      strcat(str, tmp);
+      asprintf(&str, "%s*x", str);
     }
     else {
-      sprintf(tmp, "*x**%d", i);
-      strcat(str, tmp);
-    }
-
-    if (i != p->d) {
-      sprintf(tmp, " + ");
-      strcat(str, tmp);
+      asprintf(&str, "%s*x**%d", str, i);
     }
   }
-
-  free(tmp);
   return str;
 }
 
